dc47ee0937eeeb7d9b962b15434c19c27d147c14
willp11 <williampage11@yahoo.co.uk>
2022-09-05 17:46:34 +0700

    added products

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,8 +7,12 @@ export default function Hero() {
                 <h1 className="text-[2rem] xs:text-[2.4rem] sm:text-[3.2rem] text-center font-bold tracking-tight my-4">Nova Collection Jewelry</h1>
                 <p className="text-base xs:text-lg sm:text-xl font-semibold">Your personal jeweler in Chiang Mai since 1994</p>
                 <div className="mt-8 flex items-center justify-center lg:pb-8">
-                    <button className="p-4 mr-2 w-[165px] border-2 border-blue-600 bg-blue-600 text-white font-semibold text-base sm:text-lg rounded-md hover:bg-blue-800 transition ease-in-out duration-300">Browse Shop</button>
-                    <button className="p-4 ml-2 w-[165px] border-2 border-blue-600 text-blue-600 font-semibold text-base sm:text-lg rounded-md hover:bg-blue-300 hover:text-blue-800 transition ease-in-out duration-300">Custom Jewelry</button>
+                    <button className="p-4 mr-2 w-[165px] border-2 border-blue-600 bg-blue-600 text-white font-semibold text-base sm:text-lg rounded-md hover:bg-blue-800 hover:border-blue-800 transition ease-in-out duration-300">
+                        Browse Shop
+                    </button>
+                    <button className="p-4 ml-2 w-[165px] border-2 border-blue-600 text-blue-600 font-semibold text-base sm:text-lg rounded-md hover:bg-blue-300 hover:text-blue-800 transition ease-in-out duration-300">
+                        Custom Jewelry
+                    </button>
                 </div>
             </div>
             <div className="relative w-full h-1/3 lg:w-1/2 xl:3/5 lg:h-full">
