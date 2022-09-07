380c471acd8a3d5f90787a1cf6dbe574d8521ede
willp11 <williampage11@yahoo.co.uk>
2022-09-07 16:27:19 +0700

    changed jewelry page to get products from api backend

--- nextjs-ts-ecommerce/pages/jewelry.tsx
+++ nextjs-ts-ecommerce/pages/jewelry.tsx
@@ -1,9 +1,28 @@
 import {products} from '../products/products';
 import ProductsPage from '../components/productsPage';
 import Head from 'next/head';
+import { Product } from '../types/product';
+import axios from 'axios';
+import { apiPrefix } from '../utils/apiPrefix';
 
-export default function Jewelry() {
+export async function getStaticProps() {
+    const headers = {'Content-Type': 'application/json'};
+    try {
+        const url = `${apiPrefix}product-list-get/`;
+        const res = await axios.get(url, {headers: headers});
+        const products = res.data;
+        return {
+            props: {
+                products
+            }
+        }
+    } catch(e) {
+        console.log(e);
+        return [];
+    }
+}
 
+export default function Jewelry({products}: {products: Product[]}) {
     return (
         <>
             <Head>
