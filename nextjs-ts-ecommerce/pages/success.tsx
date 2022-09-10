ea2cbdf9fc9545e8e8abeab6d8894ea4e20635c3
willp11 <williampage11@yahoo.co.uk>
2022-09-10 11:08:53 +0700

    fixed success page- flashed error as session_id could be undefined

--- nextjs-ts-ecommerce/pages/success.tsx
+++ nextjs-ts-ecommerce/pages/success.tsx
@@ -1,8 +1,8 @@
 import { useRouter } from "next/router";
-import useSWR from "swr";
-import { useEffect } from "react";
+// import useSWR from "swr";
+import { useEffect, useState } from "react";
 import { useCart } from "../hooks/useCart";
-import { CheckIcon } from "@heroicons/react/outline";
+import { CheckIcon } from "@heroicons/react/24/outline";
 import axios from 'axios';
 
 export default function Success() {
@@ -10,12 +10,24 @@ export default function Success() {
 
     const {clearCartOnSuccess} = useCart();
 
-    const fetcher = (url : string) => axios.get(url).then(res => res.data);
+    // const fetcher = (url : string) => axios.get(url).then(res => res.data);
 
-    const { data, error } = useSWR(
-        () => `/api/checkout_sessions/${session_id}`,
-        fetcher
-    )
+    // const { data, error } = useSWR(
+    //     () => `/api/checkout_sessions/${session_id}`,
+    //     fetcher
+    // )
+
+    const [data, setData] = useState();
+    const [error, setError] = useState();
+
+    useEffect(()=>{
+        if (typeof session_id !== "undefined") {
+            axios.get(`/api/checkout_sessions/${session_id}`).then(res=>{
+                setData(res.data);
+                console.log(res);
+            }).catch(err=>setError(err))
+        }
+    }, [session_id])
 
     useEffect(()=>{
         clearCartOnSuccess();
@@ -24,15 +36,15 @@ export default function Success() {
     return (
         <div className="container xl:max-w-screen-xl mx-auto py-12 px-6 text-center">
             {error ? (
-                <div className="p-2 rounded-md bg-rose-100 text-rose-500 max-w-md mx-auto dark:bg-slate-800 dark:text-gray-100">
+                <div className="p-2 rounded-md bg-rose-100 text-rose-500 max-w-md mx-auto dark:bg-slate-800">
                     <p className="text-lg">Sorry, something went wrong!</p>
                 </div>
             ) : !data ? (
-                <div className="p-2 rounded-md bg-gray-100 text-gray-500 max-w-md mx-auto dark:bg-slate-800 dark:text-gray-100">
+                <div className="p-2 rounded-md bg-white text-gray-500 max-w-md mx-auto dark:bg-slate-800">
                     <p className="text-lg animate-pulse">Loading...</p>
                 </div>
             ) : (
-                <div className="py-4 px-8 rounded-md bg-gray-100 max-w-lg mx-auto dark:bg-slate-800 dark:text-gray-100">
+                <div className="py-4 px-8 rounded-md bg-white max-w-lg mx-auto">
                     <h2 className="text-4xl font-semibold flex flex-col items-center space-x-1">
                         <CheckIcon className="w-12 h-12 flex-shrink-0 text-green-600" />
                         <span>Thanks for your order!</span>
