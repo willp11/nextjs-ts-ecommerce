8b17c62c0834ebbcea0449269ef0f9780c12e33c
willp11 <williampage11@yahoo.co.uk>
2022-12-23 03:21:23 +0700

    image domain not https

--- nextjs-ts-ecommerce/next.config.js
+++ nextjs-ts-ecommerce/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ['https://api-jewelry.williampage.me']
+        domains: ['http://api-jewelry.williampage.me']
     }
 }
 
